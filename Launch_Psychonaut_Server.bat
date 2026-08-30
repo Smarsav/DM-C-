f1fba9e330ff4172056eb01d687b8eae053b2951
Smarsav <mertburul05@gmail.com>
2026-08-30 04:19:36 +0300

    feat: one-click 1:1 PsychonautStation server and client launchers

---
+++ Launch_Psychonaut_Server.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0\psychonaut_station"
+echo ======================================================================
+echo  Building and Launching 100%% Full PsychonautStation Local Server
+echo ======================================================================
+call "%~dp0\psychonaut_station\RUN_SERVER.cmd"
